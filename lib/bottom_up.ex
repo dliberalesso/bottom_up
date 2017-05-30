e6180e06a437a4b0d2366f1fa249c47caa0f84f3
Douglas Liberalesso <dliberalesso@gmail.com>
2017-05-30 12:05:51 -0300

    Process from file, and read input string

--- lib/bottom_up.ex
+++ lib/bottom_up.ex
@@ -1,3 +1,14 @@
 defmodule BottomUp do
-  
+  alias BottomUp.Production
+
+  def process(rules_path) do
+    {start_state, productions} = Production.from rules_path
+    read_input_string()
+  end
+
+  defp read_input_string do
+    IO.gets("Informe uma cadeia:\n")
+    |> String.trim
+    |> String.split("")
+  end
 end
