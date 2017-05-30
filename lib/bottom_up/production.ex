963dcf5189610341238190911791e0816ebd5453
Douglas Liberalesso <dliberalesso@gmail.com>
2017-05-30 12:04:54 -0300

    Map productions from a file, indetifying the starter state

---
+++ lib/bottom_up/production.ex
@@ -0,0 +1,18 @@
+defmodule BottomUp.Production do
+  
+  def from(file_path), do: map_productions file_path
+
+  defp map_productions(file_path) do
+    File.read!(file_path)
+    |> String.split("\n", trim: true)
+    |> build_tuple
+  end
+
+  defp build_tuple([h | tail]), do: {h, to_map(tail)}
+
+  defp to_map(list) do
+    list
+    |> Enum.map(&String.split(&1, " -> "))
+    |> Map.new(fn [v, k] -> {k, v} end)
+  end
+end
